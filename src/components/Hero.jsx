cf7bf7f95ae39b6b4c9de2cf4b7de232106b971d
riteekagawand <riteekagawand7@gmail.com>
2025-02-03 22:22:41 +0530

    Updated resume download link in Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -166,7 +166,7 @@ const profile = {
                   className="group relative inline-flex items-center justify-center gap-3 p-0.5 rounded-xl bg-gradient-to-r from-gray-800 to-gray-700 transition-all duration-300 hover:scale-105 hover:shadow-[0_0_2rem_-0.5rem_#60A5FA]"
                 >
                   <a
-                    href="https://drive.google.com/uc?export=download&id=17f49hQwDln8KRap57gtEqlI5sKEmUJGY"
+                    href="https://drive.google.com/file/d/1Zv8k3dmPoJ8ALUXGrfG9OIbLWFUIJxZY/view?usp=sharing"
                     download
                   >
                     <span className="block w-full px-6 sm:px-8 py-3 sm:py-4 rounded-[11px] bg-gray-900 border border-gray-700/50 transition-all duration-300 group-hover:bg-gradient-to-r group-hover:from-gray-800 group-hover:to-gray-700">
